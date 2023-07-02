c36ee62512e138cc557f849ca53dab9010f2c229
amitnitjsr <amitnips007@gmail.com>
2023-07-02 16:06:48 +0530

    product status update api

--- src/main/java/com/cafe/Cafe/Management/System/dao/ProductDao.java
+++ src/main/java/com/cafe/Cafe/Management/System/dao/ProductDao.java
@@ -3,11 +3,17 @@ package com.cafe.Cafe.Management.System.dao;
 import com.cafe.Cafe.Management.System.POJO.Product;
 import com.cafe.Cafe.Management.System.wrapper.ProductWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product,Integer> {
 
     List<ProductWrapper> getAllProduct();
 
+    @Modifying
+    @Transactional
+    Integer updateProductStatus(@Param("status") String status,@Param("id") Integer id);
 }
