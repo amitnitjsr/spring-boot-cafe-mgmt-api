811923ce7bdbd7d20ccf1ed403914c7de4b73414
amitnitjsr <amitnips007@gmail.com>
2023-07-02 16:36:16 +0530

    getById product

--- src/main/java/com/cafe/Cafe/Management/System/POJO/Product.java
+++ src/main/java/com/cafe/Cafe/Management/System/POJO/Product.java
@@ -12,6 +12,9 @@ import java.io.Serializable;
 @NamedQuery(name = "Product.updateProductStatus", query = "update Product p set p.status=:status where p.id=:id")
 
 @NamedQuery(name = "Product.getProductByCategory", query = "select new com.cafe.Cafe.Management.System.wrapper.ProductWrapper(p.id,p.name) from Product p where p.category.id=:id and p.status='true'")
+
+@NamedQuery(name = "Product.getProductById", query = "select new com.cafe.Cafe.Management.System.wrapper.ProductWrapper(p.id,p.name,p.description,p.price) from Product p where p.id=:id")
+
 @Data
 @Entity
 @DynamicUpdate
