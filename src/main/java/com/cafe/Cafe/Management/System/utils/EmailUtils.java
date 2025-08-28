7ebf24f1479eacd448f07517b7348a7c3d711e78
Amit Maurya <32681070+amitnitjsr@users.noreply.github.com>
2025-08-28 21:46:34 +0530

    Update EmailUtils.java
    dummy@gmail.com

--- src/main/java/com/cafe/Cafe/Management/System/utils/EmailUtils.java
+++ src/main/java/com/cafe/Cafe/Management/System/utils/EmailUtils.java
@@ -20,7 +20,7 @@ public class EmailUtils {
     public void sendSimpleMessage(String to, String subject, String text, List<String> list){
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("amitnips007@gmail.com");
+        message.setFrom("dummy@gmail.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
@@ -41,7 +41,7 @@ public class EmailUtils {
     public void forgotMail(String to, String subject, String password) throws MessagingException{
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message,true);
-        helper.setFrom("amitnips007@gmail.com");
+        helper.setFrom("dummy@gmail.com");
         helper.setTo(to);
         helper.setSubject(subject);
         String htmlMsg = "<p><b>Your Login details for Cafe Management System</b><br><b>Email: </b> " + to + " <br><b>Password: </b> " + password + "<br><a href=\"http://localhost:4200/\">Click here to login</a></p>";
